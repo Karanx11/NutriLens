3be3b284435c4be6b5dc918b546886b71f0f6e5e
Karan Sharma <karanx11.72898@gmail.com>
2026-07-29 20:19:28 +0530

    Changing codes

--- frontend/lib/features/landing/screens/landing_screen.dart
+++ frontend/lib/features/landing/screens/landing_screen.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/hero_section.dart';
 import '../widgets/features_section.dart';
-import '../widgets/how_it_works.dart';
-import '../widgets/health_preview.dart';
-import '../widgets/testimonials.dart';
-import '../widgets/faq_section.dart';
-import '../widgets/footer.dart';
+// import '../widgets/how_it_works.dart';
+// import '../widgets/health_preview.dart';
+// import '../widgets/testimonials.dart';
+// import '../widgets/faq_section.dart';
+// import '../widgets/footer.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
