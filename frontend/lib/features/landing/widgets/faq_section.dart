771139934041f50bbb4832e0860872885cecf5ed
Karan Sharma <karanx11.72898@gmail.com>
2026-08-16 16:11:06 +0530

    Added Faq Section

--- frontend/lib/features/landing/widgets/faq_section.dart
+++ frontend/lib/features/landing/widgets/faq_section.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class FaqSection extends StatelessWidget {
+  const FaqSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
