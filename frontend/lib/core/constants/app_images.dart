3b955804a790b47cc4a9650c6fb04f3cd4748de8
Karan Sharma <karanx11.72898@gmail.com>
2026-08-17 10:04:46 +0530

    Added AppImages

---
+++ frontend/lib/core/constants/app_images.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppImages extends StatelessWidget {
+  const AppImages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
