9eb1db46160b75fcd8600a7954505815deb3e852
Karan Sharma <karanx11.72898@gmail.com>
2026-08-18 12:09:40 +0530

    Added API Constants

---
+++ frontend/lib/core/constants/api_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ApiConstants extends StatelessWidget {
+  const ApiConstants({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
